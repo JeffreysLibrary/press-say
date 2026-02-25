78dbefbac084300de3b9a60863849bcf73b00f6d
JeffreysLibrary <jeffrey.s.meyer1982@gmail.com>
2026-02-25 13:54:25 -0500

    Add files via upload

--- feelings.js
+++ feelings.js
@@ -1,12 +1,18 @@
-let a 	=document.getElementById("happy");
-let b =document.getElementById("sad");
-let c =document.getElementById("angry");
-
+let a = document.getElementById("happy");
+let b = document.getElementById("sad");
+let c = document.getElementById("angry");
+let d = document.getElementById("tired");
+let e = document.getElementById("excited");
+let f = document.getElementById("nervous");
+let g = document.getElementById("bored");
 
 a.addEventListener("click", happyGo);
 b.addEventListener("click", sadGo);
 c.addEventListener("click", angryGo);
-
+d.addEventListener("click", tiredGo);
+e.addEventListener("click", excitedGo);
+f.addEventListener("click", nervousGo);
+g.addEventListener("click", boredGo);
 
 function play(file) { 
 const audio = new Audio(file); audio.currentTime = 0; audio.play(); 
@@ -24,4 +30,21 @@ play("sad.m4a");
 
 function angryGo() {
 play("angry.m4a");	
+}
+
+
+function tiredGo() {
+play("tired.m4a");	
+}
+
+function excitedGo() {
+play("excited.m4a");	
+}
+
+function nervousGo() {
+play("nervous.m4a");	
+}
+
+function boredGo() {
+play("bored.m4a");	
 }
